35c2dc018ca6b3c9dc7bb59699bc2fe126ba587b
lucaswerner90 <wernerlucas12@gmail.com>
2021-12-14 23:54:44 +0100

    Record video every 5 episodes

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 env_name = 'SpaceInvaders-v0'
 env = gym.make(env_name)
 env = MaxAndSkipEnv(env, skip=8)
-env = gym.wrappers.Monitor(env, './videos/' + env_name, force=True, video_callable=lambda episode_id: True)
+env = gym.wrappers.Monitor(env, './videos/' + env_name, force=True, video_callable=lambda episode_id: episode_id%5==0)
 
 np.random.seed(123)
 env.seed(123)
